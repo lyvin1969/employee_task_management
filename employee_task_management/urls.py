e85795b8a71787b78640d42d57de11277801a46b
lyvin1969 <puritywandia0@gmail.com>
2026-07-20 14:15:41 +0300

    Redirect root URL to dashboard

--- employee_task_management/urls.py
+++ employee_task_management/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    # Redirect root URL '/' directly to the dashboard
+    path("", lambda request: redirect("dashboard:dashboard")),
+
     path("admin/", admin.site.urls),
 
     path("accounts/", include("accounts.urls")),
@@ -18,9 +22,7 @@ urlpatterns = [
     
     path("reports/", include("reports.urls")),
     
-   path("dashboard/", include("dashboard.urls", namespace="dashboard")),
-    
-
+    path("dashboard/", include("dashboard.urls", namespace="dashboard")),
 ]
 
 
